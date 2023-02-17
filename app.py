711a587007d4a9df4e91ace766cc0eb5306fc899
untermensch <untermenschH@protonmail.com>
2023-02-18 00:49:18 +0300

    make pdf from html using make and weasyprint libs

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+import weasyprint
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
+from data import example_data as data
+
+mylookup = TemplateLookup(directories=["templates"])
+root_path = os.path.dirname(__file__)
+css = os.path.join(root_path, "static/css/styles.css")
+
+
+def render_template(template_name, **kwargs):
+    mytemplate = mylookup.get_template(template_name)
+    return mytemplate.render(**kwargs)
+
+
+def index():
+    html_str = render_template("index.html", data=data)
+    weasyprint.HTML(string=html_str).write_pdf("output.pdf", stylesheets=[css])
+    print(f"pdf file is saved as output.pdf to {root_path}/output.pdf")
+    return html_str
+
+
+if __name__ == "__main__":
+    index()
