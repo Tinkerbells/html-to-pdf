5e45107e24c2af32c5381714b7e03c67a4cd185c
untermensch <untermenschH@protonmail.com>
2023-02-19 00:02:08 +0300

    add css and html for example data

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ import os
 import weasyprint
 from mako.template import Template
 from mako.lookup import TemplateLookup
+from flask import Flask, render_template_string
 
 from data import example_data as data
 
 mylookup = TemplateLookup(directories=["templates"])
 root_path = os.path.dirname(__file__)
 css = os.path.join(root_path, "static/css/styles.css")
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    html_str = render_template("index.html", data=data)
+    weasyprint.HTML(string=html_str).write_pdf("output.pdf", stylesheets=[css])
+    print(f"pdf file is saved as output.pdf to {root_path}/output.pdf")
+    return render_template_string(html_str)
 
 
 def render_template(template_name, **kwargs):
@@ -15,12 +25,6 @@ def render_template(template_name, **kwargs):
     return mytemplate.render(**kwargs)
 
 
-def index():
-    html_str = render_template("index.html", data=data)
-    weasyprint.HTML(string=html_str).write_pdf("output.pdf", stylesheets=[css])
-    print(f"pdf file is saved as output.pdf to {root_path}/output.pdf")
-    return html_str
-
-
 if __name__ == "__main__":
-    index()
+    # index()
+    app.run(debug=True)
