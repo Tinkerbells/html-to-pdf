3b640537c57343a99ba03612ff8f1c77de95541f
untermensch <untermenschH@protonmail.com>
2023-02-21 19:26:23 +0300

    add new chart, add dynamic age text

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import weasyprint
 from mako.template import Template
 from mako.lookup import TemplateLookup
@@ -12,16 +13,64 @@ css = os.path.join(root_path, "static/css/styles.css")
 app = Flask(__name__)
 
 
+def get_age_text(age):
+    last = int(repr(age)[-1])
+    if last == 1:
+        return "год"
+    elif last in [2, 3, 4]:
+        return "года"
+    else:
+        return "лет"
+
+
 @app.route("/")
 def index():
+    now = datetime.now()
+    formatted_date = now.strftime("%d-%m-%Y-%H_%M")
     # Разница возраста микробиоты и пациента
-    data["age_diff"] = abs(
-        data["microbiota_age"] - data["personal_data"]["patient_age"]
-    )
+    patient_age = data["personal_data"]["patient_age"]
+    microbiota_age = data["microbiota_age"]
+    diff = abs(microbiota_age - patient_age)
+
+    data["age_diff"] = diff
+    data["age_diff_text"] = get_age_text(diff)
+
+    if microbiota_age > patient_age:
+        data["first_age"] = patient_age
+        data["first_age_text"] = {
+            "text": get_age_text(patient_age),
+            "title": "возраст пациента",
+        }
+        data["second_age"] = microbiota_age
+        data["second_age_text"] = {
+            "text": get_age_text(microbiota_age),
+            "title": "возраст микробиоты",
+        }
+        data["bar_width"] = 110 - patient_age / microbiota_age * 100
+    else:
+        data["first_age"] = microbiota_age
+        data["first_age_text"] = {
+            "text": get_age_text(microbiota_age),
+            "title": "возраст микробиоты",
+        }
+        data["second_age"] = patient_age
+        data["second_age_text"] = {
+            "text": get_age_text(patient_age),
+            "title": "возраст пациента",
+        }
+        data["bar_width"] = 100 - microbiota_age / patient_age * 100
+
     html_str = render_template("index.html", data=data)
-    weasyprint.HTML(string=html_str).write_pdf("output.pdf", stylesheets=[css])
-    print(f"pdf file is saved as output.pdf to {root_path}/output.pdf")
-    return render_template_string(html_str)
+    # weasyprint.HTML(string=html_str).write_pdf("output.pdf", stylesheets=[css])
+    weasyprint.HTML(string=html_str).write_pdf(
+        f"{formatted_date}.pdf", stylesheets=[css]
+    )
+
+    print(
+        f"pdf file is saved as {formatted_date}.pdf to {root_path}/{formatted_date}.pdf"
+    )
+
+    return render_template_string(str(html_str))
 
 
 def render_template(template_name, **kwargs):
